8c751968e1ef8894a10bc00964f84308f379f189
he_wei_guang <heweiguang@zuzuche.com>
2017-11-19 15:25:17 +0800

    更多类型判断

--- src/type/index.js
+++ src/type/index.js
@@ -11,6 +11,12 @@ const number = 1,
       reg = /a/g,
       func = function a(){};
 
+function TestPlainObject(test) {
+    this.test = test;
+}
+
+const testObject = Object.create(null);
+
 function checkType() {
     const args = Array.prototype.slice.call(arguments, 0);
 
@@ -19,4 +25,37 @@ function checkType() {
     })
 }
 
+//test type
+console.log('test type');
 checkType(number, string, boolean, und, nul, obj, array, error, reg, func);
+
+//test isPlainObject
+console.log('test isPlainObject');
+console.log(Type.isPlainObject(new TestPlainObject('hello'))); //false
+console.log(Type.isPlainObject({})); //true
+console.log(Type.isPlainObject(testObject)); //true
+
+//test isEmptyObject
+console.log('test isEmptyObject');
+console.log(Type.isEmptyObject({})); //true
+console.log(Type.isEmptyObject(obj)); //false
+console.log(Type.isEmptyObject([])); //false
+
+//test isArrayLike
+console.log('test isArrayLike');
+console.log(Type.isArrayLike({
+    0: 1,
+    length: 1
+})); //true
+
+console.log(Type.isArrayLike({
+    length: 0
+})); //true
+
+console.log(Type.isArrayLike({
+    length: 1
+})); //false
+
+console.log(Type.isArrayLike({
+    a: 1
+})); //false
--- src/type/type.js
+++ src/type/type.js
@@ -2,11 +2,12 @@
  * @Author: heweiguang 
  * @Date: 2017-11-19 13:08:54 
  * @Last Modified by: heweiguang
- * @Last Modified time: 2017-11-19 13:21:51
+ * @Last Modified time: 2017-11-19 15:20:29
  */
 
  const class2type = {};
  const toString = Object.prototype.toString;
+ const hasOwn = Object.prototype.hasOwnProperty;
 
  "Boolean Number String Function Array Date RegExp Object Error Null Undefined"
     .split(' ')
@@ -27,6 +28,78 @@
             typeof obj;
  }
 
+ /**
+  * 判断一个对象是否由 {} 或 new Object 的方式创建
+  * @param {*} obj 
+  */
+ function isPlainObject(obj) {
+    let proto, 
+        Ctor;
+
+    if(!obj || toString.call(obj) !== '[object Object]') {
+        return false;
+    }
+
+    proto = Object.getPrototypeOf(obj); //原生获取 obj 的原型的方法
+
+    //没有原型代表是 plain object
+    if(!proto) {
+        return true;
+    }
+
+    //如果是 Object 创建的对象，Ctor 就等于 Object 的构造函数
+    Ctor = hasOwn.call(proto, 'constructor') && proto.constructor;
+
+    return typeof Ctor === 'function' &&
+            hasOwn.toString.call(Ctor) === hasOwn.toString.call(Object); // hasOwn.toString 是调用 Function.prototype.toString 返回一个表示函数源代码的字符串
+ }
+
+ /**
+  * 判断是否是空对象 {}
+  * @param {*} obj 
+  */
+ function isEmptyObject(obj) {
+    if(type(obj) !== 'object') {
+        return false;
+    }
+
+    for(let name in obj) {
+        return false;
+    }
+
+    return true;
+ }
+
+ /**
+  * 判断是否为 window
+  * @param {*} obj 
+  */
+ function isWindow(obj) {
+    return !!obj && obj === obj.window; //window.window 指向 window 自身
+ }
+
+ /**
+  * 判断是否为类数组
+  * @param {*} obj 
+  */
+ function isArrayLike(obj) {
+    const length = !!obj && 'length' in obj && obj.length;
+    const typeRes = type(obj);
+
+    //排除函数和 window 对象
+    if(typeRes === 'function' || isWindow(obj)) {
+        return false;
+    }
+
+    return typeRes === 'array' || //数组也返回true
+            length === 0 ||       //允许 {length: 0}  
+            typeof length === 'number' && length > 0 && (length - 1) in obj; //如果 length 大于0 那 obj[length - 1] 必须存在
+ }
+
  module.exports = {
-     type
+     type,
+     isPlainObject,
+     isEmptyObject,
+     isWindow,
+     isArrayLike
  }
